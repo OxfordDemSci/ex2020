55ae004070a93ecef0b5eeb35591a80bb3366d57
ikashnitsky <ilya.kashnitsky@gmail.com>
2021-02-26 23:06:06 +0100

    fig s3

--- src/09c-sens-e0-e60-hmd-wpp.R
+++ src/09c-sens-e0-e60-hmd-wpp.R
@@ -20,12 +20,14 @@ ids <- read_rds("{wd}/out/ids.rds" %>% glue)
 lt_85 <- read_rds("{wd}/out/lt_output_85.rds" %>% glue)
 lt_100 <- read_rds("{wd}/out/lt_output_100.rds" %>% glue)
 
-# WPP
-ex_wpp <- read_rds("{wd}/dat/wpp/wpp_ex.rds" %>% glue)
+# # WPP
+# ex_wpp <- read_rds("{wd}/dat/wpp/wpp_ex.rds" %>% glue)
+#
+# # HMD
+# lt1x1 <- read_rds(glue("{wd}/dat/hmdhfd/lt-1x1.rds"))
 
-# HMD
-lt1x1 <- read_rds(glue("{wd}/dat/hmdhfd/lt-1x1.rds"))
-
+# WPP and HMD ex estimates in lt_input
+lt_input <- read_rds("{wd}/out/lt_input_85.rds" %>% glue)
 
 # join
 ex_comp <-
@@ -37,19 +39,105 @@ ex_comp <-
     ) %>%
     left_join(ids, "code") %>%
     left_join(
-        lt1x1 %>%
-            filter(!sex == "b") %>%
+        lt_input %>%
             transmute(
-                code_hmd = country,
-                year,
-                age,
-                sex = sex %>% as_factor %>%
-                    lvls_revalue(c("Female", "Male")),
-                ex_hmd = ex
+                code = region_iso,
+                sex, year, age = age_start,
+                ex_hmd = ex_hmd_estimate,
+                ex_wpp = ex_wpp_estimate
             )
-    ) %>%
-    left_join(
-        ex_wpp %>%
-            transmute(name = Location, sex = Sex, age = AgeGrpStart, ex_wpp = ex)
     )
 
+# plot
+ex_comp %>%
+    filter(age == 0) %>%
+    ggplot(aes(year))+
+    geom_line(aes(y = ex_hmd, color = sex), alpha = .5, size = 1)+
+    geom_segment(
+        aes(y = ex_wpp, yend = ex_wpp, x = 2015, xend = 2019),
+        size = .5, color = 8
+    )+
+    geom_point(aes(y = ex_85, color = sex), shape = 95, size = 4)+
+    geom_point(aes(y = ex_100, color = sex), shape = 43, size = 2)+
+    scale_color_manual(
+        values = c("#B5223BFF", "#64B6EEFF"),
+        guide  = guide_legend(ncol = 1)
+    )+
+    scale_x_continuous(labels = c("'15", "'16", "'17", "'18", "'19", "'20"))+
+    # scale_y_continuous(breaks = 0:100)+
+    facet_wrap(~name, ncol = 7)+
+    theme_minimal(base_family = font_rc)+
+    theme(
+        legend.position = c(.9, .07),
+        panel.grid.minor = element_blank(),
+        panel.spacing.x = unit(1, "lines")
+    )+
+    labs(
+        color = NULL,
+        y = "Life expectancy estimates, years",
+        title = "Estimated yearly life expectancy at age 0 with open age group 85+ (—) and 100+ (+) compared with HMD (colored line) and WPP (grey) 5 year average estimates" %>% str_wrap(97)
+    )
+
+ggsave(
+    "{wd}/out/sens/ex-comp.pdf" %>% glue,
+    width = 8, height = 6, device = cairo_pdf
+)
+
+# # separate for sexes
+#
+# ex_comp %>%
+#     filter(age == 0, sex == "Female") %>%
+#     ggplot(aes(y = year))+
+#     geom_line(aes(x = ex_hmd, color = sex), alpha = .5, size = 1)+
+#     geom_segment(
+#         aes(x = ex_wpp, xend = ex_wpp, y = 2015, yend = 2019),
+#         size = .5, color = 8
+#     )+
+#     geom_point(aes(x = ex_85, color = sex), shape = 124, size = 3)+
+#     geom_point(aes(x = ex_100, color = sex), shape = 43, size = 2)+
+#     scale_color_manual(
+#         values = c("#B5223BFF"),
+#         guide  = guide_legend(ncol = 1)
+#     )+
+#     # scale_y_continuous(labels = c("2015", "'16", "'17", "'18", "'19", "'20"))+
+#     scale_x_continuous(breaks = 0:100)+
+#     facet_wrap(~name, ncol = 7, scales = "free_x")+
+#     theme_minimal(base_family = font_rc)+
+#     theme(
+#         legend.position = c(.9, .07),
+#         panel.grid.minor = element_blank(),
+#         panel.spacing.x = unit(1, "lines")
+#     )+
+#     labs(
+#         color = NULL,
+#         x = NULL,
+#         y = "Life expectancy estimates, years"
+#     )
+#
+# ex_comp %>%
+#     filter(age == 0) %>%
+#     ggplot(aes(year))+
+#     geom_line(aes(y = ex_hmd, color = sex), alpha = .5, size = 1)+
+#     # geom_segment(
+#     #     aes(y = ex_wpp, yend = ex_wpp, x = 2015, xend = 2019, color = "WPP"),
+#     #     alpha = .5
+#     # )+
+#     geom_point(aes(y = ex_85, color = sex), shape = 95, size = 4)+
+#     geom_point(aes(y = ex_100, color = sex), shape = 43, size = 2)+
+#     scale_color_manual(
+#         values = c("#B5223BFF", "#64B6EEFF"),
+#         guide  = guide_legend(ncol = 1)
+#     )+
+#     scale_x_continuous(labels = c("2015", "'16", "'17", "'18", "'19", "'20"))+
+#     facet_wrap(~name, ncol = 7, scales = "free")+
+#     theme_minimal(base_family = font_rc)+
+#     theme(
+#         legend.position = c(.9, .07),
+#         panel.grid.minor = element_blank(),
+#         panel.spacing.x = unit(1, "lines")
+#     )+
+#     labs(
+#         color = NULL,
+#         y = "Life expectancy estimates | Our divided by HMD"
+#     )
+#
